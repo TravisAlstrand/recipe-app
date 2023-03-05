b19f6499ba9751a3a945503e6bee0ae612dfca8f
Travis Alstrand <travisalstrand@gmail.com>
2023-03-04 22:47:12 -0800

    user model created

---
+++ api/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { Sequelize } = require('sequelize');
+const app = express();
+const router = require('./routes/index');
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'recipe-app.db'
+});
+
+app.use(express.json());
+// app.use('/api', router);
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection to database was successful!')
+  }).catch((err) => {
+    console.error('Connection to database was unsuccessful!', err);
+  });
+
+sequelize.sync()
+  .then(() => {
+    console.log("All models were synchronized successfully.");
+  }).catch(err => {
+    console.error('Model synchronization was unsuccessful!', err);
+  });
+
+
+app.listen(5000, () => {
+  console.log("App is listening on port 5000 🦄")
+});
---
+++ api/models/User.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize) => {
+
+  class User extends Model { }
+
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'A user already has this username'
+      },
+      validate: {
+        notNull: {
+          msg: 'A username is required'
+        },
+        notEmpty: {
+          msg: 'Please provide a username'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A password is required'
+        },
+        notEmpty: {
+          msg: 'Please provide a password'
+        }
+      },
+      set(val) {
+        const hashedPassword = bcrypt.hashSync(val, 10);
+        this.setDataValue('password', hashedPassword);
+      }
+    },
+  }, { sequelize });
+
+  User.associate = (models) => {
+    User.hasMany(models.Recipe, {
+      as: 'recipeCreator',
+      foreignKey: {
+        fieldName: 'userId'
+      }
+    });
+  };
+
+  return User;
+};
+
